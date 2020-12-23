d69665b90dc75a574f274d1ae16852b9d820d1d1
2544-ak <saifashahid81@gmail.com>
2020-12-23 15:18:53 +0600

    added a new function

--- synonym.py
+++ synonym.py
@@ -11,3 +11,15 @@ stemmed = ['give', 'attend']
 string = listToString(stemmed)
 
 print(string)
+
+def process_genre(string):
+    for genre in string.split(" "):
+        result = []
+        for syn in wn.synsets(genre):
+            for l in syn.lemmas():
+                result.append(l.name())
+        result = list(dict.fromkeys(result)) 
+        #result = listToString(result)
+        print(result)
+
+process_genre(string) 
